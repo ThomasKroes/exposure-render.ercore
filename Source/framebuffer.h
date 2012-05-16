9c104811abde4366546e1be4053fc8c552e823f8
Thomas Kroes <Thomas Kroes@THOMAS_PC.fritz.box>
2012-05-16 10:46:07 +0200

    - Work on buffers

--- Source/framebuffer.h
+++ Source/framebuffer.h
@@ -22,9 +22,46 @@ namespace ExposureRender
 class FrameBuffer
 {
 public:
-	FrameBuffer(void);
+	HOST FrameBuffer(void) :
+		Resolution(),
+		FrameEstimate(Enums::Device, "Frame Estimate XYZA"),
+		FrameEstimateTemp(Enums::Device, "Temp Frame Estimate XYZA"),
+		AccumulationXyza(Enums::Device, "Accumulation XYZA"),
+		Weight(Enums::Device, "Weight"),
+		RunningEstimateXyza(Enums::Device, "Running Estimate Xyza"),
+		DisplayEstimate(Enums::Device, "Display Estimate RGBA"),
+		DisplayEstimateTemp(Enums::Device, "Temp Display Estimate RGBA"),
+		DisplayEstimateFiltered(Enums::Device, "Filtered Display Estimate RGBA"),
+		RandomSeeds1(Enums::Device, "Random Seeds 1"),
+		RandomSeeds2(Enums::Device, "Random Seeds 2"),
+		RandomSeedsCopy1(Enums::Device, "Random Seeds 1 Copy"),
+		RandomSeedsCopy2(Enums::Device, "Random Seeds 2 Copy"),
+		HostDisplayEstimate(Enums::Host, "Display Estimate RGBA")
+	{
+	}
 
-	void Resize(const Vec2i& Resolution);
+	HOST void Resize(const Vec2i& Resolution)
+	{
+		if (this->Resolution == Resolution)
+			return;
+		
+		this->Resolution = Resolution;
+
+		this->FrameEstimate.Resize(this->Resolution);
+		this->FrameEstimateTemp.Resize(this->Resolution);
+		this->AccumulationXyza.Resize(this->Resolution);
+		this->Weight.Resize(this->Resolution);
+		this->RunningEstimateXyza.Resize(this->Resolution);
+		this->DisplayEstimate.Resize(this->Resolution);
+		this->DisplayEstimateTemp.Resize(this->Resolution);
+		this->DisplayEstimateFiltered.Resize(this->Resolution);
+		this->RandomSeeds1.Resize(this->Resolution);
+		this->RandomSeeds2.Resize(this->Resolution);
+		this->HostDisplayEstimate.Resize(this->Resolution);
+
+		this->RandomSeedsCopy1 = this->RandomSeeds1;
+		this->RandomSeedsCopy2 = this->RandomSeeds2;
+	}
 
 	Vec2i					Resolution;
 	Buffer2D<ColorXYZAf>	FrameEstimate;
