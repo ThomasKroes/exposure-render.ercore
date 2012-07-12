7dffe11edaa93108766c789afcd9c3ce66e67a59
Thomas <Thomas@TUD276559.tudelft.net>
2012-07-12 16:49:03 +0200

    Broken code, work on buffer generalization

--- Source/buffer.h
+++ Source/buffer.h
@@ -23,39 +23,55 @@ namespace ExposureRender
 /*! \class Buffer
  * \brief Base buffer class
  */
-template<class T>
+template<class T, int NoDimensions = 1>
 class EXPOSURE_RENDER_DLL Buffer
 {
 public:
+	/*! Default constructor */
 	HOST Buffer(const char* pName = "Untitled", const Enums::MemoryType MemoryType = Enums::Host, Enums::FilterMode FilterMode = Enums::Linear, Enums::AddressMode AddressMode = Enums::Wrap) :
 		MemoryType(MemoryType),
 		FilterMode(FilterMode),
 		AddressMode(AddressMode),
-		Data(NULL),
-		NoElements(0)
+		Data(NULL)
 	{
 		this->SetName(pName);
 	}
-
+	
+	/*! Copy constructor */
 	HOST Buffer(const Buffer& Other)
 	{
-		this->SetName(Other.GetName());
+		*this = Other;
 	}
 
+	/*! Destructor */
+	HOST virtual ~Buffer(void)
+	{
+		this->Free();
+	}
+	
+	/*! Assignment operator
+		@param[in] Other Buffer to copy from
+		* \return Copied buffer by reference
+	*/
 	HOST Buffer& operator = (const Buffer& Other)
 	{
+		this->SetName(Other.GetName());
+
 		this->FilterMode	= Other.FilterMode;
 		this->AddressMode	= Other.AddressMode;
 
+		if (this->TimeStamp != Other.TimeStamp)
+		{
+			this->Set(Other.MemoryType, Other.Resolution, Other.Data);
+			this->TimeStamp = Other.TimeStamp;
+		}
+
 		return *this;
 	}
-
+	
+	/*! Frees the memory owned by the buffer */
 	HOST void Free(void)
 	{
-		char MemoryString[MAX_CHAR_SIZE];
-
-		this->GetMemoryString(MemoryString, Enums::MegaByte);
-
 		if (this->Data)
 		{
 			if (this->MemoryType == Enums::Host)
@@ -72,67 +88,135 @@ public:
 #endif
 		}
 
-		this->Resolution	= 0;
-		this->NoElements	= 0;
+		this->Resolution = Vec<int, NoDimensions>;
 
 		this->TimeStamp.Modified();
 	}
+	
+	/*! Resets the memory owned by the buffer */
+	HOST void Reset(void)
+	{
+		if (this->Resolution.CumulativeProduct() <= 0)
+			return;
 
+		if (this->MemoryType == Enums::Host)
+			memset(this->Data, 0, this->GetNoBytes());
 
+#ifdef __CUDACC__
+		if (this->MemoryType == Enums::Device)
+			Cuda::MemSet(this->Data, 0, this->Resolution.CumulativeProduct());
+#endif
 
+		this->TimeStamp.Modified();
+	}
+	
+	/*! Resize the buffer
+		@param[in] Resolution Resolution of the buffer
+	*/
+	HOST void Resize(const Vec<int, NoDimensions>& Resolution)
+	{
+		if (this->Resolution == Resolution)
+			return;
+		else
+			this->Free();
 
+		this->Resolution = Resolution;
 
+		if (this->Resolution.CumulativeProduct() <= 0)
+			return;
 
+		char MemoryString[MAX_CHAR_SIZE];
 
+		this->GetMemoryString(MemoryString, Enums::MegaByte);
 
+		if (this->MemoryType == Enums::Host)
+		{
+			this->Data = (T*)malloc(this->GetNoBytes());
+		}
+
+#ifdef __CUDACC__
+		if (this->MemoryType == Enums::Device)
+		{
+			Cuda::Allocate(this->Data, this->Resolution.CumulativeProduct());
+		}
+#endif
+
+		this->Reset();
+	}
+
+	/*! Set the buffer
+		@param[in] MemoryType Type of memory, host/device
+		@param[in] Resolution Resolution of the buffer
+		@param[in] Data Pointer to raw data
+	*/
+	HOST void Set(const Enums::MemoryType& MemoryType, const Vec<int, NoDimensions>& Resolution, T* Data)
+	{
+		this->Resize(Resolution);
+
+		if (this->Resolution.CumulativeProduct() <= 0)
+			return;
+
+		if (this->MemoryType == Enums::Host)
+		{
+			if (MemoryType == Enums::Host)
+				memcpy(this->Data, Data, this->GetNoBytes());
+
+#ifdef __CUDACC__
+			if (MemoryType == Enums::Device)
+				Cuda::MemCopyDeviceToHost(Data, this->Data, this->Resolution.CumulativeProduct());
+#endif
+		}
+
+#ifdef __CUDACC__
+		if (this->MemoryType == Enums::Device)
+		{
+			if (MemoryType == Enums::Host)
+				Cuda::MemCopyHostToDevice(Data, this->Data, this->Resolution.CumulativeProduct());
+
+			if (MemoryType == Enums::Device)
+				Cuda::MemCopyDeviceToDevice(Data, this->Data, this->Resolution.CumulativeProduct());
+		}
+#endif
+	}
+	
+	/*! gets the buffer name
+		* \return Name of the buffer
+	*/
 	HOST const char* GetName() const
 	{
 		return this->Name;
 	}
 
+	/*! Sets the buffer name
+		@param[in] pName Name of name
+		* \return Size of the memory
+	*/
 	HOST void SetName(const char* pName)
 	{
 		sprintf_s(this->Name, MAX_CHAR_SIZE, "%s", pName);
 		this->UpdateFullName();
 	}
 	
+	/*! Gets the full name
+		* \return Full name
+	*/
 	HOST const char* GetFullName() const
 	{
 		return this->FullName;
 	}
-
-	HOST void UpdateFullName()
-	{
-		char MemoryTypeName[MAX_CHAR_SIZE];
-
-		switch (this->MemoryType)
-		{
-			case Enums::Host:
-				sprintf_s(MemoryTypeName, MAX_CHAR_SIZE, "%s", "H");
-				break;
-			
-			case Enums::Device:
-				sprintf_s(MemoryTypeName, MAX_CHAR_SIZE, "%s", "D");
-				break;
-
-			default:
-				sprintf_s(MemoryTypeName, MAX_CHAR_SIZE, "%s", "U");
-				break;
-		}
-
-		sprintf_s(this->FullName, MAX_CHAR_SIZE, "['%s', %s]", this->Name, MemoryTypeName);
-	}
-
-	HOST_DEVICE long GetNoElements(void) const
-	{
-		return this->NoElements;
-	}
-
+	
+	/*! Gets the number of bytes
+		* \return Number of bytes occupied by the buffer
+	*/
 	HOST_DEVICE virtual long GetNoBytes(void) const
 	{
-		return this->GetNoElements() * sizeof(T);
+		return this->Resolution.CumulativeProduct() * sizeof(T);
 	} 
-
+	
+	/*! Gets the memory string
+		@param[in] MemoryUnit Memory unit
+		* \return Size of the memory
+	*/
 	HOST virtual float GetMemorySize(const Enums::MemoryUnit& MemoryUnit) const
 	{
 		switch (MemoryUnit)
@@ -150,6 +234,75 @@ public:
 		return 0.0f;
 	}
 
+	/*! Gets a pointer to the data
+		* \return Pointer to raw data
+	*/
+	HOST_DEVICE T* GetData() const
+	{
+		return this->Data;
+	}
+
+	/*! Gets the memory type
+		* \return Memory type
+	*/
+	HOST Enums::MemoryType GetMemoryType() const
+	{
+		return this->MemoryType;
+	}
+
+	/*! Gets the filter mode
+		* \return Filter mode
+	*/
+	HOST Enums::FilterMode GetFilterMode() const
+	{
+		return this->FilterMode;
+	}
+
+	/*! Sets the filter mode
+		@param[in] FilterMode FilterMode
+	*/
+	HOST void SetFilterMode(const Enums::FilterMode& FilterMode)
+	{
+		this->FilterMode = FilterMode;
+	}
+
+	/*! Gets the address mode 
+		* \return Address mode
+	*/
+	HOST Enums::AddressMode GetAddressMode() const
+	{
+		return this->AddressMode;
+	}
+
+	/*! Flag the buffer as modified */
+	void Modified()
+	{
+		TimeStamp.Modified();
+	}
+
+	/*! Gets the buffer's resolution 
+		* \return Resolution
+	*/
+	HOST_DEVICE Vec<int, NoDimensions> GetResolution() const
+	{
+		return this->Resolution;
+	}
+
+	/*! Gets the number of elements in the buffer 
+		* \return Number of elements
+	*/
+	HOST_DEVICE int GetNoElements() const
+	{
+		return this->Resolution.CumulativeProduct();
+	}
+	
+	TimeStamp					TimeStamp;						/*! Time last modified */
+
+protected:
+	/*! Gets the memory string
+		@param[out] pMemoryString Memory string output
+		@param[in] MemoryUnit Memory unit
+	*/
 	HOST virtual void GetMemoryString(char* pMemoryString, const Enums::MemoryUnit& MemoryUnit = Enums::MegaByte) const
 	{
 		switch (MemoryUnit)
@@ -159,25 +312,38 @@ public:
 			case Enums::GigaByte:	sprintf_s(pMemoryString, MAX_CHAR_SIZE, "%0.2f GB", this->GetMemorySize(Enums::GigaByte));		break;
 		}
 	}
+	
+	/*! Updates the full name */
+	HOST void UpdateFullName()
+	{
+		char MemoryTypeName[MAX_CHAR_SIZE];
 
-	HOST_DEVICE T* GetData() const { return this->Data; }
-	HOST Enums::MemoryType GetMemoryType() const { return this->MemoryType; }
-	HOST Enums::FilterMode GetFilterMode() const { return this->FilterMode; }
-	HOST void SetFilterMode(const Enums::FilterMode& FilterMode) { this->FilterMode = FilterMode; }
-	HOST Enums::AddressMode GetAddressMode() const { return this->AddressMode; }
+		switch (this->MemoryType)
+		{
+			case Enums::Host:
+				sprintf_s(MemoryTypeName, MAX_CHAR_SIZE, "%s", "H");
+				break;
 
-	TimeStamp				TimeStamp;
+			case Enums::Device:
+				sprintf_s(MemoryTypeName, MAX_CHAR_SIZE, "%s", "D");
+				break;
 
-	void Modified() { TimeStamp.Modified(); }
+			default:
+				sprintf_s(MemoryTypeName, MAX_CHAR_SIZE, "%s", "U");
+				break;
+		}
 
-protected:
-	Enums::MemoryType		MemoryType;
-	Enums::FilterMode		FilterMode;
-	Enums::AddressMode		AddressMode;
-	char					Name[MAX_CHAR_SIZE];
-	char					FullName[MAX_CHAR_SIZE];
-	T*						Data;
-	int						NoElements;
+		sprintf_s(this->FullName, MAX_CHAR_SIZE, "['%s', %s]", this->Name, MemoryTypeName);
+	}
+
+
+	Enums::MemoryType			MemoryType;						/*! Type of memory e.g. host/device */
+	Enums::FilterMode			FilterMode;						/*! Type of filtering  */
+	Enums::AddressMode			AddressMode;					/*! Type of addressing  */
+	char						Name[MAX_CHAR_SIZE];			/*! Buffer name  */
+	char						FullName[MAX_CHAR_SIZE];		/*! Full buffer name  */
+	T*							Data;							/*! Pointer to raw data on host/device */
+	Vec<int, NoDimensions>		Resolution;						/*! Buffer resolution */
 };
 
 }
