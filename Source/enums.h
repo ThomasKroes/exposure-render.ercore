b7a9199a839c07755c5be180ca62ccb5a6b4a27d
Thomas Kroes <Thomas Kroes@THOMAS_PC.fritz.box>
2012-07-15 18:24:06 +0200

    Added documentation to enums

--- Source/enums.h
+++ Source/enums.h
@@ -21,156 +21,169 @@ namespace ExposureRender
 
 namespace Enums
 {
+	//! Where the memory resides
 	enum MemoryType
 	{
-		Host,
-		Device
+		Host,		// Memory resides on the host
+		Device		// Memory resides on the device
 	};
 
+	//! Memory unit
 	enum MemoryUnit
 	{
-		KiloByte,
-		MegaByte,
-		GigaByte
+		KiloByte,	// KiloByte
+		MegaByte,	// MegaByte
+		GigaByte	// GigaByte
 	};
 
+	//! Type of procedural
 	enum ProceduralType
 	{
-		Uniform = 0,
-		Checker,
-		Gradient
+		Uniform = 0,		// Uniform color accross entire procedural domain
+		Checker,			// Checkerboard
+		Gradient			// Gradient
 	};
-
+	
+	//! Type of texture
 	enum TextureType
 	{
-		Procedural = 0,
-		Bitmap
+		Procedural = 0,		// See also ::ProceduralType
+		Bitmap				// Bitmap
 	};
-
+	
+	//! Type of shape
 	enum ShapeType
 	{
-		Plane = 0,
-		Disk,
-		Ring,
-		Box,
-		Sphere,
-		Cylinder,
-		Cone
+		Plane = 0,		// Plane
+		Disk,			// Disk
+		Ring,			// Ring
+		Box,			// Box
+		Sphere,			// Sphere
+		Cylinder,		// Cylinder
+		Cone			// Cone
 	};
-
+	
+	//! Type of shading
 	enum ShadingMode
 	{
-		BrdfOnly = 0,
-		PhaseFunctionOnly,
-		Hybrid,
-		Modulation
+		BrdfOnly = 0,			// Use BRDF everywhere in the volume
+		PhaseFunctionOnly,		// Use phase function everywhere in the volume
+		Hybrid,					// Hybrid scattering
+		Modulation				// Modulation
 	};
 
+	//! Type of method for gradient computation
 	enum GradientMode
 	{
-		ForwardDifferences = 0,
-		CentralDifferences,
-		Filtered,
+		ForwardDifferences = 0,		// Forward differences
+		CentralDifferences,			// Central differences
+		Filtered,					// Filtered
 	};
 
+	//! Exception level
 	enum ExceptionLevel
 	{
-		Info = 0,
-		Warning,
-		Error,
-		Fatal
+		Info = 0,		// Info
+		Warning,		// Warning
+		Error,			// Error
+		Fatal			// Fatal
 	};
 
+	//! Emission unit, pertains to emitters
 	enum EmissionUnit
 	{
-		Power = 0,
-		Lux,
+		Power = 0,		// Emission strength per emitter
+		Lux,			// Emission strength per unit area
 	};
-
+	
+	//! Type of scattering function
 	enum ScatterFunction
 	{
-		Brdf,
-		PhaseFunction
+		Brdf,			// Didirectional reflectance distribution function
+		PhaseFunction	// Phase function
 	};
-
+	
+	//! Type of scattering
 	enum ScatterType
 	{
-		Volume			= 0x001,
-		Light			= 0x002,
-		Object			= 0x004
+		Volume			= 0x001,	// Scattering within volume
+		Light			= 0x002,	// Scattering event on light
+		Object			= 0x004		// Scattering event on object
 	};
 
+	//! Type of alignment
 	enum AlignmentType
 	{
-		AxisAlign = 0,
-		LookAt,
-		Spherical,
-		Manual
+		AxisAlign = 0,	// Align to principal axis (x, y, z)
+		LookAt,			// Using look at constraint
+		Spherical,		// Using spherical coordinates
+		Manual			// Manual alignment
 	};
 
+	//! Cartesian axes
 	enum Axis
 	{
-		X = 0,
-		Y,
-		Z
+		X = 0,		// X
+		Y,			// Y
+		Z			// Z
 	};
-
+	
+	//! Type of filtering
 	enum FilterMode
 	{
-		NearestNeighbour = 0,
-		Linear,
+		NearestNeighbour = 0,		// Nearest neighbor
+		Linear,						// Linear
 	};
-
+	
+	//! Type of focussing
 	enum FocusMode
 	{
-		AutoFocus = 0,
-		ManualFocus,
+		AutoFocus = 0,		// Auto-focus (focal distance is calculated semi-automatically)
+		ManualFocus,		// Manual focus (user provides focal distance)
 	};
 
+	//! Type of computing device
 	enum DeviceType
 	{
-		Cuda = 0,
-		OpenCL,
+		Cuda = 0,	// CUDA
+		OpenCL,		// OpenCL
 	};
 
+	//! Type of texture addressing
 	enum AddressMode
 	{
-		Wrap = 0,
-		Clamp,
-		Mirror,
-		Border
+		Wrap = 0,		// Wrap
+		Clamp,			// Clamp
+		Mirror,			// Mirror
+		Border			// Border
 	};
-
+	
+	//! Type of accelerator
 	enum AcceleratorType
 	{
-		NoAcceleration = 0,
-		Octree
+		NoAcceleration = 0,		// No acceleration
+		Octree					// Octree
 	};
 
+	//! Shape of the aperture
 	enum ApertureShape
 	{
-		Circular = 0,
-		Polygon
+		Circular = 0,		// Circular
+		Polygon				// Polygonal
 	};
 
-	enum OverlapType
-	{
-		Complete = 0,
-		Left,
-		Right,
-		Exact
-	};
-
+	//! Type of gradient
 	enum GradientType
 	{
-		Ramp = 0,
-		Radial
+		Ramp = 0,	// Ramp
+		Radial		// Radial
 	};
 
+	//! Type of rendering
 	enum RenderMode
 	{
-		StandardRayCasting,
-		StochasticRayCasting
+		StandardRayCasting,			// Standard direct volume rendering
+		StochasticRayCasting		// Stochastic direct volume rendering
 	};
 }
 
